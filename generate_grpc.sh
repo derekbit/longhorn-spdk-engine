4123b21355a4d197069734520a46b9299f7bdcd3
Shuo Wu <shuo.wu@suse.com>
2023-07-06 20:39:36 +0800

    Update generate_grpc.sh
    Signed-off-by: Shuo Wu <shuo.wu@suse.com>

--- generate_grpc.sh
+++ generate_grpc.sh
@@ -13,4 +13,19 @@ fi
 # proto lint check
 #buf check lint
 
-protoc -I proto/spdkrpc/ -I proto/vendor/protobuf/src/ proto/spdkrpc/spdk.proto --go_out=plugins=grpc:proto/spdkrpc/
+PROTO_DIR="proto/spdkrpc"
+TMP_DIR_BASE=".protobuild"
+TMP_DIR="${TMP_DIR_BASE}/github.com/longhorn/longhorn-spdk-engine/proto/"
+mkdir -p "${TMP_DIR}"
+cp -a "${PROTO_DIR}"/*.proto "${TMP_DIR}"
+for PROTO in spdk; do
+#    mkdir -p "integration/rpc/${PROTO}"
+#    python3 -m grpc_tools.protoc -I "${TMP_DIR_BASE}" -I "proto/vendor/" -I "proto/vendor/protobuf/src/" --python_out=integration/rpc/${PROTO} --grpc_python_out=integration/rpc/${PROTO} "${TMP_DIR}/${PROTO}.proto"
+    protoc -I ${TMP_DIR_BASE}/ -I proto/vendor/ -I proto/vendor/protobuf/src/ "${TMP_DIR}/${PROTO}.proto" --go_out=plugins=grpc:"${TMP_DIR_BASE}"
+    mv "${TMP_DIR}/${PROTO}.pb.go" "${PROTO_DIR}/${PROTO}.pb.go"
+
+#    mv integration/rpc/${PROTO}/github.com/longhorn/longhorn_spdk_engine/proto/${PROTO}_pb2_grpc.py integration/rpc/${PROTO}/github/com/longhorn/longhorn_spdk_engine/proto/${PROTO}_pb2_grpc.py
+#    rm -rf integration/rpc/${PROTO}/github.com
+done
+
+rm -rf "${TMP_DIR_BASE}"
